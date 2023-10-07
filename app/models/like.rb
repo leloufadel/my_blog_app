0688a544108ce442d1aa85029b76acc047716323
leloufadel <leloufadel@gmail.com>
2023-10-07 21:56:07 +0300

    fix linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,3 @@
-# class Like < ApplicationRecord
-#   belongs_to :post
-#   belongs_to :user
-
-#   def update_post_likes_counter
-#     post.update(likesCounter: post.likes.count)
-#   end
-# end
-
 class Like < ApplicationRecord
   belongs_to :post, class_name: 'Post', foreign_key: :post_id
   belongs_to :user, class_name: 'User', foreign_key: :user_id
