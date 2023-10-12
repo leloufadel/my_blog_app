7c9c17e0424790b9c98bda831121f457eea119eb
leloufadel <leloufadel@gmail.com>
2023-10-12 20:02:27 +0300

    add authorization role

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     resources :posts, only: %i[index show new create destroy] do
       resources :comments, only: %i[new create destroy]
       resources :likes, only: %i[new create]
+      resources :posts, only: %i[index show new create destroy] do
+        resources :comments, only: %i[new create destroy]
     end
+  end
   end
 end
