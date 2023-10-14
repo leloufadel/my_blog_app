749be171eec85369d1ffe64ab1dc274cd77b479b
leloufadel <leloufadel@gmail.com>
2023-10-14 10:09:01 +0300

    fix linters

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,31 +1,31 @@
 module Api
-    class CommentsController < ApplicationController
-        before_action :set_post
-        before_action :authenticate_api, except: [:index]
-      
-      def index
-        @comments = @post.comments
-        render json: @comments
-      end
-  
-      def create
-        @comment = @post.comments.build(comment_params)
-        @comment.user = current_user
-        if @comment.save
-          render json: @comment, status: :created
-        else
-          render json: @comment.errors, status: :unprocessable_entity
-        end
-      end
-  
-      private
-  
-      def set_post
-        @post = Post.find(params[:post_id])
-      end
-  
-      def comment_params
-        params.require(:comment).permit(:text)
+  class CommentsController < ApplicationController
+    before_action :set_post
+    before_action :authenticate_api, except: [:index]
+
+    def index
+      @comments = @post.comments
+      render json: @comments
+    end
+
+    def create
+      @comment = @post.comments.build(comment_params)
+      @comment.user = current_user
+      if @comment.save
+        render json: @comment, status: :created
+      else
+        render json: @comment.errors, status: :unprocessable_entity
       end
     end
+
+    private
+
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
   end
+end
--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,17 +1,17 @@
 module Api
-    class PostsController < ApplicationController
-        before_action :set_user
-        before_action :authenticate_api, except: [:index]
-      
-      def index
-        @posts = @user.posts
-        render json: @posts
-      end
-  
-      private
-  
-      def set_user
-        @user = User.find(params[:user_id])
-      end
+  class PostsController < ApplicationController
+    before_action :set_user
+    before_action :authenticate_api, except: [:index]
+
+    def index
+      @posts = @user.posts
+      render json: @posts
+    end
+
+    private
+
+    def set_user
+      @user = User.find(params[:user_id])
     end
   end
+end
